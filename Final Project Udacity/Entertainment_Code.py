dcad27edb52996d62c78de2fc3796bb6be239334
micesqueektwice88 <31232621+micesqueektwice88@users.noreply.github.com>
2017-10-15 09:29:40 -0700

    Add files via upload

---
+++ Final Project Udacity/Entertainment_Code.py
@@ -0,0 +1,8 @@
+import media
+import fresh_tomatoes
+
+Blade_runner.Movie("Blade_runner"
+                    "Sci-fi future"
+                    "Detective that hunts androids"
+                    "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg"
+                    "https://www.youtube.com/watch?v=eogpIG53Cis")
