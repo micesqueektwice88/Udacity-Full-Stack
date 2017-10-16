cda46d8cd23bde1b6fb92b2fd5f52996554e5a01
micesqueektwice88 <31232621+micesqueektwice88@users.noreply.github.com>
2017-10-15 18:29:50 -0700

    Add files via upload

--- Final Project Udacity/Entertainment_Code.py
+++ Final Project Udacity/Entertainment_Code.py
@@ -6,3 +6,15 @@ Blade_runner.Movie("Blade_runner"
                     "Detective that hunts androids"
                     "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg"
                     "https://www.youtube.com/watch?v=eogpIG53Cis")
+
+Dredd.Movie("Dredd"
+            "A Dystopian Future"
+            "Crazy future cop"
+            "https://en.wikipedia.org/wiki/Dredd#/media/File:Dredd2012Poster.jpg"
+            "https://www.youtube.com/watch?v=G-eI5oLlIeY")
+
+Requiem_for_a_Dream.Movie("Requim for a Dream"
+                          "Heroin Addicts"
+                          "https://en.wikipedia.org/wiki/Requiem_for_a_Dream"
+                          "https://www.youtube.com/watch?v=yVIRcnlRKF8")
+
