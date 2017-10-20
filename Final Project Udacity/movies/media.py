acbcd9c789226ab1fd6e5923037113c7b6c8ff34
micesqueektwice88 <31232621+micesqueektwice88@users.noreply.github.com>
2017-10-19 21:27:26 -0700

    Update media.py

--- Final Project Udacity/movies/media.py
+++ Final Project Udacity/movies/media.py
@@ -3,7 +3,12 @@ import webbrowser
 class Movie():
     """This is a way to store movie information"""
 
-    VARIOUS_RATINGS = ["G", "PG", "PG-13", "R", "NC-17"]
+    VARIOUS_RATINGS = [
+        "G", 
+        "PG", 
+        "PG-13", 
+        "R", 
+        "NC-17"]
     
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
